466a1c3111f96039dc3ce96886510499226bcc32
BrunnoFdc <brunoribeiro.sch@gmail.com>
2020-08-15 18:02:56 -0300

    Add: Compiled app

---
+++ precache-manifest.48c58ce56396084846dec2783e343cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68baef6ed39e84cbee39288666fcebcc",
+    "url": "/div-img-render/index.html"
+  },
+  {
+    "revision": "f89ef62380f1f3960434",
+    "url": "/div-img-render/static/css/main.4b869d17.chunk.css"
+  },
+  {
+    "revision": "dd189d63551618220d13",
+    "url": "/div-img-render/static/js/2.edcc4917.chunk.js"
+  },
+  {
+    "revision": "d143d24d23e49c077c6463d8c5b8cdbd",
+    "url": "/div-img-render/static/js/2.edcc4917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89ef62380f1f3960434",
+    "url": "/div-img-render/static/js/main.f1f5fe64.chunk.js"
+  },
+  {
+    "revision": "1e1f76c4d92e1c7fe3c3",
+    "url": "/div-img-render/static/js/runtime-main.f80ddba6.js"
+  }
+]);
